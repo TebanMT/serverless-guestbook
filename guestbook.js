9e592790a8d58422ef94c039b5fc5cfe59d76a2f
TebanMT <38414277+TebanMT@users.noreply.github.com>
2022-10-04 22:29:12 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -41,6 +41,10 @@ const guestbook = {
     console.log('Loading entries...');
     $('#entries').html('Loading entries...');
     guestbook.get().done(function(result) {
+      console.log("Hola")
+      console.log(result)
+      console.log(result.body)
+      console.log(context)
       if (!result.body) {
         return;
       }
@@ -48,9 +52,6 @@ const guestbook = {
       const context = {
         entries: result.body
       }
-      console.log(result)
-      console.log(result.body)
-      console.log(context)
       $('#entries').html(entriesTemplate(context));
     }).error(function(error) {
       $('#entries').html('No entries');
